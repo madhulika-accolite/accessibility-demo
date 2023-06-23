21c4cdae740e732ff073447239d2b23a2b836582
Madhulika Mukherjee <madhulika.m@accolite.com>
2023-06-23 22:47:34 +0530

    access demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+import ReactFC from 'react-fusioncharts';
+import Accessibility from './assets/fusioncharts.ext.accessibility';
+import AccessibilityString from './assets/fusioncharts.ext.accessibility-resources-strings-en';
+
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme, Accessibility, AccessibilityString);
+
+FusionCharts.Accessibility.setAlternateTexts({
+  'chartobject-1': {
+      'chart': 'Este es un gráfico {chartType}',
+      'plot': '{plotIndex}. {label}, {formattedValue}.'
+  }
+});
+
+const dataSource = {
+  chart: {
+    caption: 'Countries With Most Oil Reserves [2017-18]',
+    subCaption: 'In MMbbl = One Million barrels',
+    xAxisName: 'Country',
+    yAxisName: 'Reserves (MMbbl)',
+    numberSuffix: 'K',
+    theme: 'fusion',
+    showValues:1
+  },
+  data: [
+    { label: 'Venezuela', value: '290' },
+    { label: 'Saudi', value: '260' },
+    { label: 'Canada', value: '180' },
+    { label: 'Iran', value: '140' },
+    { label: 'Russia', value: '115' },
+    { label: 'UAE', value: '100' },
+    { label: 'US', value: '30' },
+    { label: 'China', value: '30' }
+  ]
+};
+
+const chartConfigs = {
+  type: 'column2d',
+  width: 600,
+  height: 400,
+  dataFormat: 'json',
+  dataSource: dataSource
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactFC {...chartConfigs} />
     </div>
   );
 }
